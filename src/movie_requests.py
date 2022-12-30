6bb7a0432da9ab9422f546f57a3358da3e059ed5
sam-sommerer <sommerer@usc.edu>
2022-12-30 14:56:01 -0800

    create get_movie_info function

---
+++ src/movie_requests.py
@@ -0,0 +1,11 @@
+import requests
+
+from constants import OMDB_API_KEY
+
+OMDB_URL = "http://www.omdbapi.com"
+
+
+def get_movie_info(payload: dict[str, str]) -> dict[str, str]:
+    payload["apikey"] = OMDB_API_KEY
+    r: requests.models.Response = requests.get(OMDB_URL, params=payload)
+    return r.json()
