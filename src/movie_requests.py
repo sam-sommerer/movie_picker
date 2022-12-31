a88d07dcba30f4842b77a748b41d1df7c0a306d2
sam-sommerer <sommerer@usc.edu>
2022-12-30 23:16:55 -0800

    make all movie extracted info strings

--- src/movie_requests.py
+++ src/movie_requests.py
@@ -17,9 +17,9 @@ def extract_db_information(response: dict[str, Any]) -> Any:
 
     movie_db_attr["imdb_id"] = response["imdbID"]
     movie_db_attr["title"] = response["Title"]
-    movie_db_attr["year"] = int(response["Year"])
+    movie_db_attr["year"] = response["Year"]
     movie_db_attr["rating"] = response["Rated"]
-    movie_db_attr["runtime"] = int(response["Runtime"].split()[0])
+    movie_db_attr["runtime"] = response["Runtime"].split()[0]
     movie_db_attr["plot"] = response["Plot"]
 
     rotten_tomato_rating: Optional[int] = None
@@ -27,9 +27,9 @@ def extract_db_information(response: dict[str, Any]) -> Any:
     for rating in response["Ratings"]:
         if rating.get("Source") is not None:
             if rating.get("Source") == "Rotten Tomatoes":
-                rotten_tomato_rating = int(rating.get("Value")[:-1])
+                rotten_tomato_rating = rating.get("Value")[:-1]
             elif rating.get("Source") == "Internet Movie Database":
-                imdb_rating = float(rating.get("Value").split("/")[0])
+                imdb_rating = rating.get("Value").split("/")[0]
     movie_db_attr["rotten_tomato_rating"] = rotten_tomato_rating
     movie_db_attr["imdb_rating"] = imdb_rating
 
